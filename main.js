1770ae3d22b7be6d5423910e6ca45cdc4b547aba
Piorek1994 <pprus94@gmail.com>
2022-04-25 12:55:20 +0200

    JS for each

---
+++ main.js
@@ -0,0 +1,34 @@
+const arrows = document.querySelectorAll('.arrow')
+const answer1 = document.querySelector('.answ1')
+const answer2 = document.querySelector('.answ2')
+const answer3 = document.querySelector('.answ3')
+const answer4 = document.querySelector('.answ4')
+const answer5 = document.querySelector('.answ5')
+
+
+
+// const showAnswer = () => {
+//     answer1.classList.toggle = 'block'
+// }
+
+
+// arrows.forEach(e => {
+//    arrows.addEventListener('click', showAnswer)
+//    console.log(e);
+
+// });
+
+
+
+
+
+
+
+
+
+
+
+// arrows.addEventListener('click', showAnswer)
+
+
+
